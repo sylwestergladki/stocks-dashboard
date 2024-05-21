f81556c166179d72e98616d3b3580dca7e4d024a
SG <sylwester.gladki@gmail.com>
2024-05-21 19:56:18 +0200

    add basic frontend module

---
+++ FrontendModule/stocks-dashboard-frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>STOCKS DASHBOARD APPLICATION</h2>
+  <ChoiceForm/>
+</template>
+
+<script>
+import ChoiceForm from './components/ChoiceForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ChoiceForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ FrontendModule/stocks-dashboard-frontend/src/components/ChoiceForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Symbol Form</h1>
+    <form @submit.prevent="submitForm">
+      <label for="symbol">Symbol:</label><br>
+      <input type="text" id="symbol" v-model="symbol" required /><br /><br />
+
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      symbol: ''
+    };
+  },
+  methods: {
+    submitForm() {
+      // Do something with the symbol value, for example, log it
+      console.log('Symbol:', this.symbol);
+    }
+  }
+};
+</script>
